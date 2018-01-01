cc31648f392985bb142880d892ea3ab19b7c5920
Michael Serpieri <michael.serpieri@pj.ca>
2018-01-01 14:15:59 -0500

    Bump version: 0.5.0
    A good subset of the API is now available
    The core seems stable enough to handle the Atlas API

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atlasapi',
-    version='0.0.1',
+    version='0.5.0',
     python_requires='>=3.5',
     packages=find_packages(),
     install_requires=['requests', 'python-dateutil'],

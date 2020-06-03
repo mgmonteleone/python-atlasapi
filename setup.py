705f98053c024ea4a0cf69454bdc4dffe06e1b1b
mgm <mgm@mgms.eu>
2020-06-03 14:20:56 +0200

    Updated for 0.11.1 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='atlasapi',
-    version='0.11.0',
+    version='0.11.1',
     python_requires='>=3.6',
     packages=find_packages(exclude=("tests",)),
     install_requires=['requests', 'python-dateutil', 'isodate', 'future', 'pytz','coolname', 'nose'],
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     entry_points={
         'console_scripts': [
